3bef508c62e9589857dab5663a4eb02d21fdab71
George Dobrin <georgedobrin11@gmail.com>
2019-01-06 11:42:24 +0200

    renamed files again

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 importScripts('sw-toolbox.js'); 
 
-toolbox.precache(["2_1077_DOBRIN_George.html","2_1077_DOBRIN_George.css","2_1077_DOBRIN_George.js"]); 
+toolbox.precache(["index.html","style.css","script.js"]); 
 toolbox.router.get('/media/*', toolbox.cacheFirst); 
 toolbox.router.get('/lib/*', toolbox.cacheFirst); 
 toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
